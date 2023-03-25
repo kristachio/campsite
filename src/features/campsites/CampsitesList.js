0552646c47d8acbe7a8d0d91a968f68f06efa637
Krista Frietas <krista.frietas@gmail.com>
2023-03-24 23:14:21 -0400

    CampstieDetailPage with useParams

--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -13,7 +13,7 @@ const CampsitesList = ({setCampsiteId}) => {
                         md='5' 
                         className='m-4' 
                         key={campsite.id}
-                        onClick={() => setCampsiteId(campsite.id)}
+                        
                         >
                         <CampsiteCard campsite={campsite} />
                     </Col>
