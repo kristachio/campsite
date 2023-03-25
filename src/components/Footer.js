138e0d4a42a25c534f4624cc3657bee70dbfc369
Krista Frietas <krista.frietas@gmail.com>
2023-03-24 22:30:27 -0400

    Header and Footer Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from 'reactstrap';
-
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -7,34 +7,48 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col xs={{ size: 4, offset: 1 }} sm='2'>
-                        <h5>this will be site navigation!</h5>
-                    </Col> 
+                        <h5>Links</h5>
+                        <ul className='list-unstyled'>
+                            <li>
+                                <Link to='/'>Home</Link>
+                            </li>
+                            <li>
+                                <Link to='/directory'>Directory</Link>
+                            </li>
+                            <li>
+                                <Link to='/about'>About</Link>
+                            </li>
+                            <li>
+                                <Link to='/contact'>Contact</Link>
+                            </li>
+                        </ul>
+                    </Col>
                     <Col xs='6' sm='3' className='text-center'>
                         <h5>Social</h5>
-                            <a
-                                className='btn btn-social-icon btn-instagram'
-                                href='http://instagram.com/'
-                            >
-                                <i className='fa fa-instagram' />
-                            </a>{' '}
-                            <a
-                                className='btn btn-social-icon btn-facebook'
-                                href='http://www.facebook.com/'
-                            >
-                                <i className='fa fa-facebook' />
-                            </a>{' '}
-                            <a
-                                className='btn btn-social-icon btn-twitter'
-                                href='http://twitter.com/'
-                            >
-                                <i className='fa fa-twitter' />
-                            </a>{' '}
-                            <a
-                                className='btn btn-social-icon btn-google'
-                                href='http://youtube.com/'
-                            >
-                                <i className='fa fa-youtube' />
-                            </a>
+                        <a
+                            className='btn btn-social-icon btn-instagram'
+                            href='http://instagram.com/'
+                        >
+                            <i className='fa fa-instagram' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-facebook'
+                            href='http://www.facebook.com/'
+                        >
+                            <i className='fa fa-facebook' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-twitter'
+                            href='http://twitter.com/'
+                        >
+                            <i className='fa fa-twitter' />
+                        </a>{' '}
+                        <a
+                            className='btn btn-social-icon btn-google'
+                            href='http://youtube.com/'
+                        >
+                            <i className='fa fa-youtube' />
+                        </a>
                     </Col>
                     <Col sm='4' className='text-center'>
                         <a
@@ -55,7 +69,7 @@ const Footer = () => {
                     </Col>
                 </Row>
             </Container>
-        </footer>   
+        </footer>
     );
 };
 
