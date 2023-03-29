4dd5c44006995cecc0f2dc513e812ee57f80cc05
Krista Frietas <krista.frietas@gmail.com>
2023-03-29 11:46:50 -0400

    Contact Page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -24,19 +24,19 @@ const AboutPage = () => {
                     <Card>
                         <CardHeader className='bg-primary text-white'>
                             <h3>Facts at a Glance</h3>
-                                <CardBody>
-                                    <dl className='row'>
-                                        <dt className='col-6'>Founded</dt>
-                                        <dd className='col-6'>February 3, 2016</dd>
-                                        <dt className='col-6'>No. of Campsites in 2019</dt>
-                                        <dd className='col-6'>563</dd>
-                                        <dt className='col-6'>No. of Reviews in 2019</dt>
-                                        <dd className='col-6'>4388</dd>
-                                        <dt className='col-6'>Employees</dt>
-                                        <dd className='col-6'>42</dd>
-                                    </dl>   
-                                </CardBody>
                         </CardHeader>
+                        <CardBody>
+                            <dl className='row'>
+                                <dt className='col-6'>Founded</dt>
+                                <dd className='col-6'>February 3, 2016</dd>
+                                <dt className='col-6'>No. of Campsites in 2019</dt>
+                                <dd className='col-6'>563</dd>
+                                <dt className='col-6'>No. of Reviews in 2019</dt>
+                                <dd className='col-6'>4388</dd>
+                                <dt className='col-6'>Employees</dt>
+                                <dd className='col-6'>42</dd>
+                            </dl>   
+                        </CardBody>
                     </Card>
                 </Col>
                 <Col>
